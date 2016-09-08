32c88ec0a7c1848c9670fe1e5cbb7d23e6d815ee
YllkaGojani <yllka.gojani@gmail.com>
2016-09-08 13:39:34 +0200

    Added the files for the Names python assignment,part 1 and 2.

---
+++ partTwo.py
@@ -0,0 +1,22 @@
+users = {
+ 'Students': [ 
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+for key,data in users.items():
+ 	print key
+ 	number = 0
+
+  	for value in data:
+  		number += 1
+  		full_name = (value["first_name"] + " " + value["last_name"]).upper()
+		length = len(value["first_name"]) + len(value["last_name"])
+		print str(number) +"-"+ full_name +"-"+ str(length)
